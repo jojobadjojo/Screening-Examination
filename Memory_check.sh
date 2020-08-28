7f9b88106d68ff5e8d36cf0cdc6620ad9527b062
Jojo <jojomaldos.6@gmail.com>
2020-08-29 01:15:28 +0800

    TRIAL1

--- Memory_check.sh
+++ Memory_check.sh
@@ -1,61 +1,108 @@
-#!/bin/bash
+		#!/bin/bash
 
-u_MemSpace=$(free | grep -i Mem | awk '{print $3/$2*100}')
-usage(){
-	echo "USAGE: $0 [-c 90] [-w 60] [-e <email address>]"
-	exit 2;
-}
-function crit_func() {
-	if [ $u_MemSpace -ge 90 ] && [ $u_MemSpace -le 100 ]; then
-		echo "ALERT: Memory Space is in CRITICAL level ($u_MemSpace %)"
-		echo "Sending the top 10 processes that used most of your Memory Space to the emal you provided. . . ."
+		u_MemSpace_dbl=$(free | grep -i Mem | awk '{print $3/$2*100}')
+		u_MemSpace=${u_MemSpace_dbl%.*}
+		usage(){
+			echo "USAGE: $0 [-c 90] [-w 60] [-e <email address>]"
+			exit 2;
+		}
+		######################### M A I N #################################
+		while getopts 'c:w:e:' opt ; 
+		do
+			case $opt in
+				c) c=$OPTARG;;
+				w) w=$OPTARG;;
+				e) e=$OPTARG;;
+				*) usage ;;
+			esac
+		done
 
-		e_Msg=/home/ec2-user/top10.txt
-		top -b | awk '!(NR<=6) {print} | head -10' > $e_Msg
+		echo $c $w $e
+
+
+		if [ $c -eq 90 ] && [ $w -eq 60 ]; then
+				if [ $u_MemSpace -ge 9 ] && [ $u_MemSpace -le 100 ]; then
+					echo "ALERT: Memory Space is in $u_MemSpace%. Status: CRITICAL!"
+					echo "Sending the top 10 processes that used most of your Memory Space to the email you provided. . . ."
+					e_Sender=MemCheck.TechExam@gmail.com
+			
+			
+					#e_Msg=$(top -b | awk '!(NR<=6) {print}' | head -10)
+				
+					e_Subj=$(date +'%Y%m%d %H:%M')
 		
-		e_Subj=$(date + '%Y%m%D %H:%M')
-		echo "PLease see attached file" | mail -s "$e_Subj" $e -A $e_Msg
-	else
-		echo "Memory Space is in $u_MemSpace %. Status: OKAY"
-		exit 0;
-}
 
-function warn_func() {
-	if [ $u_MemSpace -ge 60 ] && [ $u_MemSpace -le 90 ]; then
-		echo "ALERT: Memory Space is in CRITICAL level ($u_MemSpace %)"
-	else
-		echo "Memory Space is in $u_MemSpace %. Status: OKAY"
-		exit 0;
+					{
+						echo To: $e
+						echo From: $e_Sender
+						echo Subject: $e_Subj
+						echo -e Hi Team,\n\nPlease see the details below.\n\n
+							top -b | awk '!(NR<=6) {print}' | head -10
+						echo -e \n\nThank you.
+					} | ssmtp $e
+					echo "Email Sent!"
 
-}
+				elif [ $u_MemSpace -ge 6 ] && [ $u_MemSpace -le 9 ]; then
+					echo "ALERT: Memory Space is $u_MemSpace%. Status: WARNING"
+				else
+					echo "Memory Space is $u_MemSpace %. Status: OKAY"
+					exit 0;
+			    fi
+		else
+			usage
+		fi
+		
+		#!/bin/bash
 
+		u_MemSpace_dbl=$(free | grep -i Mem | awk '{print $3/$2*100}')
+		u_MemSpace=${u_MemSpace_dbl%.*}
+		usage(){
+			echo "USAGE: $0 [-c 90] [-w 60] [-e <email address>]"
+			exit 2;
+		}
+		######################### M A I N #################################
+		while getopts 'c:w:e:' opt ; 
+		do
+			case $opt in
+				c) c=$OPTARG;;
+				w) w=$OPTARG;;
+				e) e=$OPTARG;;
+				*) usage ;;
+			esac
+		done
 
-######################### M A I N #################################
-while getopts 'c:w:e:' opt ; 
-do
-	case $opt in
-		c) c=$OPTARG;;
-		w) w=$OPTARG;;
-		e) e=$OPTARG;;
-		*) usage ;;
-	esac
-done
-
-[ -z "$c"] && usage; [ -z "$w"] && usage; [ -z "$e"] && usage
-
-
-if [ $c -e 90 ]; then
-	crit_func
-else
-	usage
-fi
-
-if [ $w -e 60 ]; then
-	warn_func
-else
-	usage
-fi
-
+		echo $c $w $e
 
 
+		if [ $c -eq 90 ] && [ $w -eq 60 ]; then
+				if [ $u_MemSpace -ge 9 ] && [ $u_MemSpace -le 100 ]; then
+					echo "ALERT: Memory Space is in $u_MemSpace%. Status: CRITICAL!"
+					echo "Sending the top 10 processes that used most of your Memory Space to the email you provided. . . ."
+					e_Sender=MemCheck.TechExam@gmail.com
+			
+			
+					#e_Msg=$(top -b | awk '!(NR<=6) {print}' | head -10)
+				
+					e_Subj=$(date +'%Y%m%d %H:%M')
+		
 
+					{
+						echo To: $e
+						echo From: $e_Sender
+						echo Subject: $e_Subj
+						echo -e Hi Team,\n\nPlease see the details below.\n\n
+							top -b | awk '!(NR<=6) {print}' | head -10
+						echo -e \n\nThank you.
+					} | ssmtp $e
+					echo "Email Sent!"
+
+				elif [ $u_MemSpace -ge 6 ] && [ $u_MemSpace -le 9 ]; then
+					echo "ALERT: Memory Space is $u_MemSpace%. Status: WARNING"
+				else
+					echo "Memory Space is $u_MemSpace %. Status: OKAY"
+					exit 0;
+			    fi
+		else
+			usage
+		fi
+		
