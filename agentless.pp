1b1c2db1e1405af3814335aeef5f28e2256ff7c7
Jojo <jojomaldos.6@gmail.com>
2020-08-30 11:53:12 +0800

    TRIAL 2

--- agentless.pp
+++ agentless.pp
@@ -4,7 +4,7 @@ node 'bpx.server.local' {
         include  curl
         include add_user
         include directories
-        include git_dl
+        include src_link
 }
 
 class  vim{
@@ -33,12 +33,24 @@ class directories {
         file { '/home/monitor/scripts/':
                 ensure => 'directory',
                 owner => 'monitor',
-                mode => '0750',
+                mode => '0755',
+        }
+        exec { 'get-mem-check':
+                command => '/bin/wget https://raw.githubusercontent.com/jojobadjojo/Screening-Examination/master/memory_check',
+                path => "/home/monitor/scripts"
+                owner => 'monitor',
+                mode => '0755',
         }
 }
-class git_dl {
-        exec { 'get-mem-check':
-                command => '/bin/wget https://raw.githubusercontent.com/jojobadjojo/Screening-Examination/master/Memory_check.sh',
-                path => "/home/monitor/scripts"
+class src_link {
+        file { '/home/monitor/src/':
+                ensure => 'directory',
+                owner => 'monitor',
+                mode => '0755',
+        }
+        file { '/home/scripts/memory_check'
+        	ensure => 'link'
+        	target =>  '/home/monitor/src/my_memory_check'
+
         }
 }
